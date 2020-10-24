987f6fda35b929e9de1522c1c4c6d2e276f0b961
HjortFogh <AlexanderHjortFogh@live.dk>
2020-10-24 20:54:41 +0200

    test

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-window.onload=function() {
-	canvas = document.getElementById("game_screen");
-	ctx = canvas.getContext("2d");
-
-	console.log("Logged");
-
-	ctx.fillRect(20, 20, 100, 100);
-}
+//Main
